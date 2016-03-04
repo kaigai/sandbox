c7c574d449042f4edd2e8f88e02a82d00d9578a8
KaiGai Kohei <kaigai@kaigai.gr.jp>
2016-03-04 17:39:10 +0900

    a workaround

--- Rstat/pgsql-kmeans.r
+++ Rstat/pgsql-kmeans.r
@@ -19,6 +19,15 @@ PgParam_dbname <- "chembl"
   }
 }
 
+# just for my convenience
+vatts_full <- c("c1","c2","c3","c4","c5","c6","c7","c8","c9","c10",
+                "c11","c12","c13","c14","c15","c16","c17","c18","c19","c20",
+                "c21","c22","c23","c24","c25","c26","c27","c28","c29","c30",
+                "c31","c32","c33","c34","c35","c36","c37","c38","c39","c40",
+                "c41","c42")
+vatts_small <- c("c1","c2","c3","c4","c5","c6","c7","c8","c9","c10")
+
+
 #
 # pgsql_kmeans - returns pair of id and cluster for each item
 #
@@ -29,78 +38,81 @@ PgParam_dbname <- "chembl"
 # n_clusters - number of clusters (integer)
 # threshold - maximum distance to continue k-means repeat (float, default: 0.0)
 #
+pgsql_kmeans_tryblock <- function(conn, relname, att_pk, att_val,
+                                  n_clusters, threshold=0.0)
+{
+  #
+  # Init: construction of pg_temp.cluster_map based on random
+  #
+  sql1 <- "SELECT " || att_pk || ", " ||
+              "(random() * " || n_clusters || ")::int + 1 cid " ||
+            "INTO pg_temp.cluster_map " ||
+            "FROM " || relname
+  print(sql1)
+
+  #
+  # Init: construction of pg_temp.centroid according to the cluster_map
+  #
+  sql2 <- "SELECT cid"
+  for (att in att_val)
+  {
+    sql2 <- sql2 || ", avg(" || att || ") " || att
+  }
+  sql2 <- sql2 || " INTO pg_temp.centroid " ||
+                  " FROM pg_temp.cluster_map c, " || relname || " r " ||
+                  "WHERE c." || att_pk || " = r." || att_pk ||
+                  " GROUP BY cid"
+  print(sql2)
+
+  #
+  # Repeat: calculation of the distance between each item and centroid,
+  #         then item shall belong to the closest cluster on the next
+  #
+  sql3 <- "SELECT " || att_pk || ", cid INTO pg_temp.cluster_map_new " ||
+            "FROM (SELECT row_number() OVER w rank, " ||
+                         "r." || att_pk || ", c.cid, " ||
+                         "sqrt("
+  is_first <- 1
+  for (att in att_val) 
+  {
+    sql3 <- sql3 || ifelse(is_first, "", " + ") ||
+            "(r." || att || " - c." || att || ")^2"
+    is_first <- 0
+  }
+  sql3 <- sql3 || ") dist FROM " || relname || " r, centroid c) new_dist " ||
+          "WINDOW w AS (PARTITION BY " || att_pk ||" ORDER BY dist)) foo " ||
+          "WHERE rank = 1"
+  print(sql3)
+
+  #
+  # Repeat: check differences between cluster_map and cluster_map_new
+  #
+  sql4 <- "SELECT count(*) FROM (" ||
+          "SELECT * FROM pg_temp.cluster_map " ||
+          "EXCEPT ALL " ||
+          "SELECT * FROM pg_temp.cluster_map_new" ||
+          ") diff"
+  print(sql4)
+
+  #
+  # Repeat: if SQL4 has any result, cluster_map_new is renamed to
+  #
+  sql5a <- "DROP TABLE pg_temp.cluster_map"
+  sql5b <- "ALTER TABLE pg_temp.cluster_map_new RENAME TO cluster_map"
+  sql5c <- "VACUUM ANALYZE pg_temp.cluster_map"
+
+  
+
+}
+
 pgsql_kmeans <- function(relname, att_pk, att_val, n_clusters, threshold=0.0)
 {
   # Open the database connection
   conn <- dbConnect(PostgreSQL(),
                     host=PgParam_hostname,
                     dbname=PgParam_dbname)
-  tryCatch({
-    #
-    # Init: construction of pg_temp.cluster_map based on random
-    #
-    sql1 <- "SELECT " || att_pk || ", " ||
-                "(random() * " || n_clusters || ")::int + 1 cid " ||
-              "INTO pg_temp.cluster_map " ||
-              "FROM " || relname
-    print(sql1)
-
-    #
-    # Init: construction of pg_temp.centroid according to the cluster_map
-    #
-    sql2 <- "SELECT cid"
-    for (att in att_val)
-    {
-      sql2 <- sql2 || ", avg(" || att || ") " || att
-    }
-    sql2 <- sql2 || " INTO pg_temp.centroid " ||
-                    " FROM pg_temp.cluster_map c, " || relname || " r " ||
-                    "WHERE c." || att_pk || " = r." || att_pk
-    print(sql2)
-
-    #
-    # Repeat: calculation of the distance between each item and centroid,
-    #         then item shall belong to the closest cluster on the next
-    #
-    sql3 <- "SELECT " || att_pk || ", cid INTO pg_temp.cluster_map_new " ||
-              "FROM (SELECT row_number() OVER w rank, " ||
-                           "r." || att_pk || ", c.cid, "
-                           "sqrt("
-    is_first <- 1
-    for (att in att_val) 
-    {
-      sql3 <- sql3 || ifelse(is_first, "", " + ") ||
-              "(r." || att || " - c." || att || ")^2"
-      is_first <- 0
-    }
-    sql3 <- ") dist FROM " || relname || " r, centroid c) new_dist " ||
-            "WINDOW w AS (PARTITION BY " || att_pk ||" ORDER BY dist)) foo " ||
-            "WHERE rank = 1"
-    print(sql3)
-
-    #
-    # Repeat: check differences between cluster_map and cluster_map_new
-    #
-    sql4 <- "SELECT * FROM pg_temp.cluster_map " ||
-            "EXCEPT ALL " ||
-            "SELECT * FROM pg_temp.cluster_map_new"
-    print(sql4)
-
-
-    #
-    # Repeat: if SQL4 has any result, cluster_map_new is renamed to
-    #
-    sql5a <- "DROP TABLE pg_temp.cluster_map"
-    sql5b <- "ALTER TABLE pg_temp.cluster_map_new RENAME TO cluster_map"
-    sql5c <- "VACUUM ANALYZE pg_temp.cluster_map"
-
-    # Goto SQL2, and restart the calculation again
-
-  },
-  finnaly = {
-    # clean up the database session
-    dbDisconnect(conn)
-  })
+  e <- try(pgsql_kmeans_tryblock(conn,relname, att_pk, att_val,
+                                 n_clusters, threshold))
+  # clean up database session on error
+  dbDisconnect(conn)
 }
-
-
--- Rstat/pgsql-minmax.r
+++ Rstat/pgsql-minmax.r
@@ -19,6 +19,14 @@ PgParam_dbname <- "chembl"
   }
 }
 
+# just for my convenience
+vatts_full <- c("c1","c2","c3","c4","c5","c6","c7","c8","c9","c10",
+                "c11","c12","c13","c14","c15","c16","c17","c18","c19","c20",
+                "c21","c22","c23","c24","c25","c26","c27","c28","c29","c30",
+                "c31","c32","c33","c34","c35","c36","c37","c38","c39","c40",
+                "c41","c42")
+vatts_small <- c("c1","c2","c3","c4","c5","c6","c7","c8","c9","c10")
+
 #
 # pgsql_minmax - returns id and distance of the items
 #
@@ -28,81 +36,87 @@ PgParam_dbname <- "chembl"
 # att_val - vector of the property columns (string[])
 # nloops  - number of iteration (integer)
 #
+pgsql_minmax_tryblock <- function(conn, relname, att_pk, att_val, nloops)
+{
+  dbGetInfo(conn)
+
+  # Init: construction of 'subset_table'
+  # ------------------------------------
+  sql1 <- "SELECT * INTO pg_temp.subset_table FROM " || relname ||
+          " ORDER BY random() LIMIT 1"
+  print("Init SQL Run: " || sql1)
+  dbGetQuery(conn, sql1)
+
+  # Init: construction of 'dist_table'
+  # ----------------------------------
+  sql2 <- "SELECT r." || att_pk || ", sqrt("
+  is_first <- 1
+  for (att in att_val)
+  {
+    sql2 <- sql2 || ifelse(is_first, "", " + ") ||
+            "(r." || att || " - s." || att || ")^2"
+    is_first <- 0
+  }
+  sql2 <- sql2 || ") dist " ||
+          "INTO pg_temp.dist_table " ||
+          "FROM (SELECT * FROM pg_temp.subset_table LIMIT 1) s, " ||
+          relname || " r"
+  print("Init SQL Run: " || sql2)
+  dbGetQuery(conn, sql2)
+
+  # Repeat: pick a largest distance item, compute new distance,
+  #         then store the least one
+  # -----------------------------------------------------------
+  sql3 <- "SELECT r1." || att_pk || ", LEAST(d.dist, sqrt("
+  is_first <- 1
+  for (att in att_val)
+  {
+    sql3 <- sql3 || ifelse(is_first, "", " + ") ||
+            "(r1." || att || " - r2." || att || ")^2"
+    is_first <- 0
+  }    
+  sql3 <- sql3 || ")) dist " ||
+          "INTO pg_temp.dist_table_new " ||
+          "FROM " || relname || " r1, " ||
+                "pg_temp.dist_table d, " ||
+               "(SELECT main.* FROM " || relname || " main, " ||
+                     "(SELECT id FROM pg_temp.dist_table " ||
+                            "ORDER BY dist DESC LIMIT 1) largest " ||
+         "WHERE main." || att_pk || " = largest." || att_pk || ") r2 " ||
+         "WHERE r1." || att_pk || " = d." || att_pk
+  sql3a <- "DROP TABLE pg_temp.dist_table"
+  sql3b <- "ALTER TABLE pg_temp.dist_table_new RENAME TO dist_table"
+
+  print("Repeat SQL is: " || sql3)
+
+  for (i in 1:nloops)
+  {
+    print(as.character(i) || "th repeat")
+    dbGetQuery(conn, sql3)
+    dbGetQuery(conn, sql3a)
+    dbGetQuery(conn, sql3b)
+  }
+
+  # Final: obtain the final result from the dist_table
+  # --------------------------------------------------
+  sql4 <- "SELECT * FROM pg_temp.dist_table"
+  res <- dbGetQuery(conn, sql4)  
+
+  # clean up the database session
+  dbDisconnect(conn)
+
+  return(res)
+}
+
 pgsql_minmax <- function(relname, att_pk, att_val, nloops)
 {
   # Open the database connection
   conn <- dbConnect(PostgreSQL(),
                     host=PgParam_hostname,
                     dbname=PgParam_dbname)
-  tryCatch({
-    # Init: construction of 'subset_table'
-    # ------------------------------------
-    sql1 <- "SELECT * INTO pg_temp.subset_table FROM " || relname ||
-            " ORDER BY random() LIMIT 1"
-    ## dbGetQuery(conn, sql1)
-    sql1a <- "VACUUM ANALYZE pg_temp.subset_table"
-    ## dbGetQuery(conn, sql1a)
-
-    # Init: construction of 'dist_table'
-    # ----------------------------------
-    sql2 <- "SELECT r." || att_pk || ", sqrt("
-    is_first <- 1
-    for (att in att_val)
-    {
-      sql2 <- sql2 || ifelse(is_first, "", " + ") ||
-              "(r." || att || " - s." || att || ")^2"
-      is_first <- 0
-    }
-    sql2 <- sql2 || ") dist " ||
-            "INTO pg_temp.dist_table " ||
-            "FROM pg_temp.subset_table s, " || relname || " r"
-    ## dbGetQuery(conn, sql2)
-
-    # Repeat: pick a largest distance item, compute new distance,
-    #         then store the least one
-    # -----------------------------------------------------------
-    sql3 <- "SELECT r1." || att_pk || ", LEAST(d.dist, sqrt("
-    is_first <- 1
-    for (att in att_val)
-    {
-      sql3 <- sql3 || ifelse(is_first, "", " + ") ||
-              "(r1." || att || " - r2." || att || ")^2"
-      is_first <- 0
-    }    
-    sql3 <- sql3 || ")) dist " ||
-            "INTO pg_temp.dist_table_new " ||
-            "FROM " || relname || " r1, " ||
-                  "pg_temp.dist_table d, " ||
-                 "(SELECT main.* FROM " || relname || " main, " ||
-                       "(SELECT id FROM pg_temp.dist_table " ||
-                              "ORDER BY dist DESC LIMIT 1) largest " ||
-           "WHERE main." || att_pk || " = largest." || att_pk || ") r2 " ||
-           "WHERE r1." || att_pk || " = d." || att_pk
-    sql3a <- "DROP TABLE pg_temp.dist_table"
-    sql3b <- "ALTER TABLE pg_temp.dist_table_new RENAME TO dist_table"
-
-    for (i in 1:nloops)
-    {
-      print(as.character(i) || "th repeat")
-      ## dbGetQuery(conn, sql3)
-      ## dbGetQuery(conn, sql3a)
-      ## dbGetQuery(conn, sql3b)
-    }
-
-    # Final: obtain the final result from the dist_table
-    # --------------------------------------------------
-    sql4 <- "SELECT * FROM pg_temp.dist_table"
-    res <- dbGetQuery(conn, sql4)  
-
-    # clean up the database session
-    dbDisconnect(conn)
-
-    return(res)  
-  },
-  finnaly = {
-    # clean up the database session
-    dbDisconnect(conn)
-  })
+  e <- try(pgsql_minmax_tryblock(conn, relname, att_pk, att_val, nloops))
+  # clean up database session on error
+  dbDisconnect(conn)
 }
 
 #
